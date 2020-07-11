877b2c273237856ef413fd2e81580ebf544da3cb
yawnoc <yawnoc@pm.me>
2020-07-11 22:53:42 +0800

    Retain old version of image_read_greyscale
    Remove the stupid try/catch cleanup body though.

--- image_read_greyscale.m
+++ image_read_greyscale.m
@@ -1,7 +1,7 @@
 ## image_ = image_read_greyscale (file_name)
 ##
-## Read an image from file to a matrix of greyscale intensities.
-## Natural range [0, 1] is used.
+## Read image from file and convert to double-precision greyscale.
+##
 ## Assumes the read image is either RGB (for which rgb2gray will work)
 ## or already greyscale (for which rgb2gray will raise an error).
 
@@ -13,7 +13,6 @@ function image_ = image_read_greyscale (file_name)
   try
     image_ = rgb2gray (image_);
   catch
-    image_ = image_;
   end_try_catch
   
 endfunction
