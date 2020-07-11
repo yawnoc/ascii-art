6030134173ab727b71b2c24fc51df3d70b908692
yawnoc <yawnoc@pm.me>
2020-07-11 23:48:52 +0800

    Add image resize function

---
+++ image_resize.m
@@ -0,0 +1,24 @@
+## new_image = image_resize (old_image, [new_height, new_width], method)
+##
+## Resize a greyscale image using interpolation.
+## Default method is "cubic", usually called 'bicubic'.
+##
+## Available methods are those for the built-in interp2.
+
+function new_image = image_resize (old_image, new_size, method = "cubic")
+  
+  old_size = size (old_image);
+  old_height = old_size(1);
+  old_width = old_size(2);
+  
+  new_height = new_size(1);
+  new_width = new_size(2);
+  
+  new_x_coordinates = linspace (1, old_width, new_width);
+  new_y_coordinates = linspace (1, old_height, new_height);
+  
+  [new_x_grid, new_y_grid] = meshgrid (new_x_coordinates, new_y_coordinates);
+  
+  new_image = interp2 (old_image, new_x_grid, new_y_grid, method);
+  
+endfunction
