294d41323c0a8c9842bddddebb83beacbda829a4
yawnoc <yawnoc@pm.me>
2020-07-12 20:37:06 +0800

    Remove files from old version

--- old/matrix_average.m
+++
@@ -1,9 +0,0 @@
-## average = matrix_average (matrix)
-##
-## Compute the average of all elements of a matrix.
-
-function average = matrix_average (matrix)
-  
-  average = mean (matrix(:));
-  
-endfunction
